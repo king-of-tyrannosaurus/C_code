368802dce581d9770079f32fde39982dce97b521
king-of-tyrannosaurus <2195965372@qq.com>
2025-08-10 18:47:32 +0800

    除法/，取余%

--- C_2025_8_10/test.c
+++ C_2025_8_10/test.c
@@ -4,19 +4,20 @@
 int main()
 {
     int a,b;
-    scanf("%d %d",&a,&b);
     
-    if(a <= 0 || a>10000 || b <= 0 || b>10000)
+    scanf("%d %d",&a,&b);//读取输入规则
+    
+    if(a <= 0 || a>10000 || b <= 0 || b>10000)// ||逻辑或符号，任意一个为真则为真
     {
         printf("范围不正确");
         return 1;
     }
     
     int x,y;
-    
     x = a/b;//得整数部分
     y = a%b;//得余数部分
     
-    printf("%d %d\n",x,y);
+    printf("%d %d\n",x,y);//打印规则
+
     return 0;
 }
