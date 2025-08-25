270d3d76eab15f1907937778aca1f86f762387d3
king-of-tyrannosaurus <2195965372@qq.com>
2025-08-25 18:34:36 +0800

    do while循环

---
+++ C_2025_8_25/test.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+//do..while循环
+int main()
+{
+    int i = 0;
+    
+    do
+    {
+        if(i == 5)
+            continue;
+        
+        printf("haha\n");
+        i++;
+    }
+    while (i<10);
+   
+    return 0;
+}
